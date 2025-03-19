3948dd256299dc6da7201bbe8962775eb0992787
BluTargaryan <opeyemiyolo@gmail.com>
2025-03-19 18:57:21 +0000

    i contentfuul, test integration and fetching of data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-'use client';
+
+import { createClient } from 'contentful';
 
 import LatestSection from "./components/Home/sections/LatestSection";
 import TrendingSection from "./components/Home/sections/TrendingSection";
@@ -6,11 +7,21 @@ import TopPostsByCategorySection from "./components/Home/sections/TopPostsByCate
 import AdSpace from "./components/global/sections/AdSpace";
 import HomeCategoryList from "./components/Home/sections/HomeCategoryList";   
 
+export async function getPosts() {
+  const client = createClient({
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID || '',
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN || '',
+  });
+  const data: any = await client.getEntries({
+    content_type: 'post',
+  });
+  console.log({
+    items: data.items[0].fields,
+  })
+}
 
-export default function PageContainer() {
-
-
-
+export default async function PageContainer() {
+await getPosts()
 
   return (
     <main className="w-full overflow-x-hidden scroll-smooth flex flex-col gap-16 py-8">
